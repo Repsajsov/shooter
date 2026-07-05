0ccd242ab1392dd12d5f76a652526570cf896986
Repsajsov <jaspervos12@outlook.com>
2026-07-05 03:19:03 +0200

    RayCollisions aan het testen.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
+#include <iostream>
+
 #include "raylib.h"
 #include "raymath.h"
+
+int const SCREEN_WIDTH = 800;
+int const SCREEN_HEIGHT = 450;
 
 struct InputState
 {
@@ -8,6 +13,7 @@ struct InputState
   bool moveLeft;
   bool moveRight;
   Vector2 mouseDelta;
+  bool shoot;
 
   void gatherInput();
 };
@@ -19,6 +25,7 @@ void InputState::gatherInput()
   moveLeft = IsKeyDown(KEY_A);
   moveRight = IsKeyDown(KEY_Q);
   mouseDelta = GetMouseDelta();
+  shoot = IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
 }
 
 void updateLook(Camera& camera, InputState& input, float& yaw, float& pitch)
@@ -76,10 +83,32 @@ void updatePosition(Camera& camera, InputState& input, float yaw, float dt)
   camera.position.z += movement.z * moveSpeed * dt;
 }
 
+void updateShooting(Camera& camera, InputState& input, BoundingBox& targetBox)
+{
+  if (!input.shoot) return;
+
+  Ray ray;
+  ray.position = camera.position;
+  ray.direction =
+      Vector3Normalize(Vector3Subtract(camera.target, camera.position));
+
+  RayCollision collision = GetRayCollisionBox(ray, targetBox);
+  if (collision.hit) std::cout << "HIT!" << std::endl;
+}
+
+void drawCrosshair()
+{
+  int const size = 10;
+
+  DrawLine((SCREEN_WIDTH / 2) - size, (SCREEN_HEIGHT / 2),
+           (SCREEN_WIDTH / 2) + size, (SCREEN_HEIGHT / 2), BLACK);
+  DrawLine((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2) - size, (SCREEN_WIDTH / 2),
+           (SCREEN_HEIGHT / 2) + size, BLACK);
+}
 
 int main()
 {
-  InitWindow(800, 450, "shooter");
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "shooter");
   SetTargetFPS(144);
   DisableCursor();
 
@@ -96,23 +125,40 @@ int main()
   float pitch = 0.0f;
   float dt = 0.0f;
 
+
   while (!WindowShouldClose())
   {
     dt = GetFrameTime();
     input.gatherInput();
 
+    Vector3 redBox;
+    redBox.x = 0.0f;
+    redBox.y = 1.0f;
+    redBox.z = 0.0f;
+    float redBoxSize = 2.0f;
+
+    BoundingBox box;
+    box.min = (Vector3){redBox.x - redBoxSize / 2, redBox.y - redBoxSize / 2,
+                        redBox.z - redBoxSize / 2};
+    box.max =
+        Vector3Add(box.min, (Vector3){redBoxSize, redBoxSize, redBoxSize});
+
     updateLook(camera, input, yaw, pitch);
     updatePosition(camera, input, yaw, dt);
+    updateShooting(camera, input, box);
 
     BeginDrawing();
     ClearBackground(RAYWHITE);
 
+
     BeginMode3D(camera);
+
     DrawPlane((Vector3){0.0f, 0.0f, 0.0f}, (Vector2){20.0f, 20.0f}, LIGHTGRAY);
-    DrawCube((Vector3){0.0f, 1.0f, 0.0f}, 2.0f, 2.0f, 2.0f, RED);
+    DrawCube(redBox, redBoxSize, redBoxSize, redBoxSize, RED);
     DrawCubeWires((Vector3){0.0f, 1.0f, 0.0f}, 2.0f, 2.0f, 2.0f, MAROON);
     DrawGrid(20, 1.0f);
     EndMode3D();
+    drawCrosshair();
 
     DrawFPS(10, 10);
     EndDrawing();
