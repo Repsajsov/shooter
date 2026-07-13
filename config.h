793ec06a6cfafe971a73e72f9f8cfe8117719e91
Repsajsov <jaspervos12@outlook.com>
2026-07-13 21:35:49 +0200

    Had de stages per ongeluk als commit messages gedaan.

--- config.h
+++ config.h
@@ -5,6 +5,6 @@ int constexpr SCREEN_WIDTH = 1600;
 int constexpr SCREEN_HEIGHT = 900;
 int constexpr DAMAGE_PER_HIT = 20;
 int constexpr FPS = 144;
-int constexpr FOV = 75;
+int constexpr FOV = 120;
 
 #endif
