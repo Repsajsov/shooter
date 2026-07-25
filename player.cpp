8f43b6379b2f9f87e5fb9991fad3300e53524129
Repsajsov <jaspervos12@outlook.com>
2026-07-26 01:55:06 +0200

    De camera beetje hoger gezet, dit wordt misschien ook wel een waarde vanuit de json file.

--- player.cpp
+++ player.cpp
@@ -15,8 +15,8 @@ float horizontalToVerticalFOV(float hFOVDegrees, float aspectRatio)
 
 Player::Player()
 {
-  camera.position = Vector3{0.0f, 1.0f, 10.0f};
-  camera.target = Vector3{0.0f, 2.0f, 0.0f};
+  camera.position = Vector3{0.0f, 5.0f, 10.0f};
+  camera.target = Vector3{0.0f, 5.0f, 0.0f};
   camera.up = Vector3{0.0f, 1.0f, 0.0f};
   camera.fovy = horizontalToVerticalFOV(FOV, ASPECT_RATIO);
   camera.projection = CAMERA_PERSPECTIVE;
